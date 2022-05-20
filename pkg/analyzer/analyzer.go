8586885e2fa60a3320bc767da64ce5c58123f7bd
Konstantin Prokopenko <kprokopenko@gmail.com>
2022-05-20 21:25:42 +0300

    Refactoring

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -18,26 +18,40 @@ var Analyzer = &analysis.Analyzer{
 	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
-func exprHasFuncWithName(exprs []ast.Expr, fnnames []string) bool {
-	for _, result := range exprs {
-		if call, ok := result.(*ast.CallExpr); ok {
-			if fnsel, ok := call.Fun.(*ast.SelectorExpr); ok {
-				for _, fnname := range fnnames {
-
-					if fnsel.Sel.Name == fnname {
-						return true
-					}
-				}
-			}
-		}
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
 	}
 
-	return false
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		funcDecl := node.(*ast.FuncDecl)
+
+		if !hasErrors(funcDecl.Type.Results) {
+			return
+		}
+
+		unwrappedErrorReturns := CountUnwrappedReturns(funcDecl.Body)
+
+		if len(unwrappedErrorReturns) == 1 { // one unwrapped error is not an issue
+			return
+		}
+
+		for _, pos := range unwrappedErrorReturns {
+			pass.Reportf(pos, "unwrapped error")
+		}
+	})
+
+	return nil, nil
 }
 
-func exprHasNil(exprs []ast.Expr) bool {
-	for _, result := range exprs {
-		if ident, ok := result.(*ast.Ident); ok && ident.Name == "nil" {
+func hasErrors(results *ast.FieldList) bool {
+	if results == nil {
+		return false
+	}
+
+	for _, result := range results.List {
+		if ident, ok := result.Type.(*ast.Ident); ok && ident.Name == "error" {
 			return true
 		}
 	}
@@ -61,80 +75,28 @@ func CountUnwrappedReturns(body *ast.BlockStmt) []token.Pos {
 	return rets
 }
 
-func run(pass *analysis.Pass) (interface{}, error) {
-	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	nodeFilter := []ast.Node{
-		(*ast.FuncDecl)(nil),
-	}
-
-	inspector.Preorder(nodeFilter, func(node ast.Node) {
-		funcDecl := node.(*ast.FuncDecl)
-
-		res := funcDecl.Type.Results
-		if res == nil {
-			return
-		}
-
-		has := false
-		for _, result := range res.List {
-			if ident, ok := result.Type.(*ast.Ident); ok {
-				has = ident.Name == "error"
-				break
+func exprHasFuncWithName(exprs []ast.Expr, fnnames []string) bool {
+	for _, result := range exprs {
+		if call, ok := result.(*ast.CallExpr); ok {
+			if fnsel, ok := call.Fun.(*ast.SelectorExpr); ok {
+				for _, fnname := range fnnames {
+					if fnsel.Sel.Name == fnname {
+						return true
+					}
+				}
 			}
 		}
+	}
 
-		if !has {
-			return
+	return false
+}
+
+func exprHasNil(exprs []ast.Expr) bool {
+	for _, result := range exprs {
+		if ident, ok := result.(*ast.Ident); ok && ident.Name == "nil" {
+			return true
 		}
+	}
 
-		positions := CountUnwrappedReturns(funcDecl.Body)
-		if len(positions) < 2 {
-			return
-		}
-
-		for _, pos := range positions {
-			pass.Reportf(pos, "unwrapped error")
-		}
-
-		// params := funcDecl.Type.Params.List
-		// if len(params) < 2 { // [0] must be format (string), [1] must be args (...interface{})
-		// 	return
-		// }
-
-		// formatParamType, ok := params[len(params)-2].Type.(*ast.Ident)
-		// if !ok { // first param type isn't identificator so it can't be of type "string"
-		// 	return
-		// }
-
-		// if formatParamType.Name != "string" { // first param (format) type is not string
-		// 	return
-		// }
-
-		// if formatParamNames := params[len(params)-2].Names; len(formatParamNames) == 0 || formatParamNames[len(formatParamNames)-1].Name != "format" {
-		// 	return
-		// }
-
-		// argsParamType, ok := params[len(params)-1].Type.(*ast.Ellipsis)
-		// if !ok { // args are not ellipsis (...args)
-		// 	return
-		// }
-
-		// elementType, ok := argsParamType.Elt.(*ast.InterfaceType)
-		// if !ok { // args are not of interface type, but we need interface{}
-		// 	return
-		// }
-
-		// if elementType.Methods != nil && len(elementType.Methods.List) != 0 {
-		// 	return // has >= 1 method in interface, but we need an empty interface "interface{}"
-		// }
-
-		// if strings.HasSuffix(funcDecl.Name.Name, "f") {
-		// 	return
-		// }
-
-		// pass.Reportf(node.Pos(), "printf-like formatting function '%s' should be named '%sf'",
-		// 	funcDecl.Name.Name, funcDecl.Name.Name)
-	})
-
-	return nil, nil
+	return false
 }
